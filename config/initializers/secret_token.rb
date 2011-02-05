6f3db11c975ce3ab44dda9f93fa29f93ba5eef60
Dia Kharrat <dkharrat@gmail.com>
2011-02-05 00:08:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SetiWorld::Application.config.secret_token = 'ac44fccbf6de671432e7129f447a9fb4c745f29024173672f70494cf8e0df2d4129ed6c3bb7bb14bbd65f3abc3b3efeb7acf744804c3b20d6c2c070af5a40351'
