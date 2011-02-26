cc956752706787b85b6c10920303858534c216ec
anthonyt <anthony.tang@west.cmu.edu>
2011-02-26 10:28:13 -0800

    open sourced header added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,41 @@
 /*
+################################################################################
+#
+# File:    application.js
+# Project: World of SETI (WOS)
+# Authors:     
+#                  Alan Mak
+#                  Anthony Tang
+#                  Dia Kharrat
+#                  Paul Wong
+# 
+# The initial source was worked on by students of Carnegie Mellon Silicon Valley
+#
+# Copyright 2011 The SETI Institute
+#
+# World of SETI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# World of SETI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with World of SETI.  If not, see<http://www.gnu.org/licenses/>.
+# 
+# Implementers of this code are requested to include the caption
+# "Licensed through SETI" with a link to setiQuest.org.
+# 
+# For alternate licensing arrangements, please contact
+# The SETI Institute at www.seti.org or setiquest.org. 
+#
+################################################################################
+*/
+
+/*
  * This code searches for all the <script type="application/processing" target="canvasid">
  * in your page and loads each script in the target canvas with the proper id.
  * It is useful to smooth the process of adding Processing code in your page and starting
